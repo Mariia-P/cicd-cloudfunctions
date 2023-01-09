b85de763edacf48aea3588c113b2ca4e0069d283
Mariia Podhorna <user@Users-MacBook-Pro.local>
2023-01-09 21:30:43 +0200

    New commit

--- index.js
+++ index.js
@@ -5,6 +5,6 @@
 //-----------------------------------------------------------------------------
 
 exports.helloWorld = (req, res) => {
-    const message="<font color='blue'>Hello СloudFunction from!</font><br><b>App Version 1.1</b>";
+    const message="<font color='blue'>Hello СloudFunction from Staging!</font><br><b>App Version 1.1</b>";
     res.status(200).send(message);
   };
