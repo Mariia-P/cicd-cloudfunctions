0767703b16bb194b21865699b69fa5e593dcd4d7
Mariia Podhorna <user@Users-MacBook-Pro.local>
2023-01-10 20:15:39 +0200

    Test

--- index.js
+++ index.js
@@ -5,6 +5,6 @@
 //-----------------------------------------------------------------------------
 
 exports.helloWorld = (req, res) => {
-    const message="<font color='blue'>Hello СloudFunction from Staging main branch!!!</font><br><b>App Version 1.5</b>";
+    const message="<font color='blue'>Hello СloudFunction from MAIN branch!!!</font><br><b>App Version 1.5</b>";
     res.status(200).send(message);
   };
