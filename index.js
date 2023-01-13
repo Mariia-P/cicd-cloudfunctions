ca71045047d2c66735d7b801f5d45ea82bd2edac
Mariia Podhorna <user@Users-MacBook-Pro.local>
2023-01-13 08:12:54 +0200

    Comment

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-
-
-//-----------------------------------------------------------------------------
-// NodeJS App for GCP Cloud Functions deployed via GCP Cloud Build Triggers
-//-----------------------------------------------------------------------------
-
 exports.helloWorld = (req, res) => {
     const message="<font color='blue'>Hello СloudFunction from Staging!</font><br><b>App Version 1.1</b>";
     res.status(200).send(message);
