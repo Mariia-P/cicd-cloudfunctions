0e2415a8c45be0124f39242bfd705502b5fa8a47
Mariia Podhorna <user@Users-MacBook-Pro.local>
2023-01-13 09:28:18 +0200

    Test

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 exports.helloWorld = (req, res) => {
-    const message="<font color='blue'>Hello СloudFunction from Staging!</font><br><b>App Version 1.1</b>";
+    const message="<font color='blue'>Hello СloudFunction from Staging NEW TEXT!</font><br><b>App Version 1.1</b>";
     res.status(200).send(message);
   };
